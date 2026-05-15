38b319d265e1840e1201caf6a42c5f2bb51495a2
Christine Schuhmann <173158012+tine-schreibt@users.noreply.github.com>
2026-05-15 14:19:07 +0200

    Complete manual reversion to v0.4.10, just with discernAndSetSystemFolderState updated to respect no-css-blah rule

--- plugin/src/settingsTab.ts
+++ plugin/src/settingsTab.ts
@@ -60,10 +60,6 @@ export class TextFlowSettingsTab extends PluginSettingTab {
           desc.createSpan({
             text: this.plugin.t("setSystemFolder.setDesc.2 how to enter root"),
           });
-          desc.createEl("br");
-          desc.createSpan({
-            text: "20:25",
-          });
         }),
       );
 
